7043bca526093ddf08caf331d6087a0d148a5725
Ford Scott <scottc99@students.rowan.edu>
2017-09-26 22:11:02 -0400

    updated homework with test cases

--- Java_homework/src/edu/cofc/csci230/ArrayList.java
+++ Java_homework/src/edu/cofc/csci230/ArrayList.java
@@ -52,7 +52,7 @@ public class ArrayList<AnyType extends Comparable<AnyType>> implements List<AnyT
     public void add(int index, AnyType t) throws IndexOutOfBoundsException {
     		if(index<0 || index>=size) {
 			throw new IndexOutOfBoundsException("Index "+index+" is out of bounds");
-		} 
+    		} 
     		if(size>=capacity)
     			grow();
 
@@ -185,17 +185,17 @@ public class ArrayList<AnyType extends Comparable<AnyType>> implements List<AnyT
      * 
      */
     private void grow() {	
-    		int i = 0;
-    		int cap = capacity;            
+    	int i = 0;
+    	int cap = capacity;            
     		
         AnyType tempArr[] = (AnyType[]) new Comparable[ cap*2 ];
 
-    		while(i<cap) {
-    			tempArr[i] = array[i];
-    			capacity = capacity+1;
-    			i++;
-    		}
-    		array = tempArr;
+    	while(i<cap) {
+    		tempArr[i] = array[i];
+   			capacity = capacity+1;
+   			i++;
+   		}
+   		array = tempArr;
     		//System.out.print(tempArr.length+"\n");
     		/**
          * -------------------------------------------
@@ -217,10 +217,10 @@ public class ArrayList<AnyType extends Comparable<AnyType>> implements List<AnyT
      * 
      */
     private void shrink() {
-    		int i = 0;
-    		int cap = capacity;            
+    	int i = 0;
+    	int cap = capacity;            
 		
-    		AnyType tempArr[] = (AnyType[]) new Comparable[ cap ];
+   		AnyType tempArr[] = (AnyType[]) new Comparable[ cap ];
 
 		while(i<cap/2) {
 			tempArr[i] = array[i];
@@ -286,36 +286,104 @@ public class ArrayList<AnyType extends Comparable<AnyType>> implements List<AnyT
          
         ArrayList<Integer> arrList = new ArrayList<Integer>();
         
-        for(int i=0; i<5; i++) {
+    // Test Case 1: Add 10 integers into the list and print the size of the list and its contents
+        System.out.print("Test case 1:\n");
+    	
+        for(int i=0; i<10; i++) {
+            arrList.add(i);
+        }
+        System.out.print(arrList.toString()+"\n");
+        
+    // Test Case 2: Clear the ArrayList out completely, then print the size of the list and its contents
+        System.out.print("\nTest case 2:\n");
+        
+        arrList.clear();
+        System.out.print(arrList.toString()+"\n");
+
+    // Test Case 3: Add 10 integers into the list, then add an integer at a specific index
+        System.out.print("\nTest case 3:\n");        
+        
+        for(int i=0; i<10; i++) {
             arrList.add(i);
         }
         
-        System.out.print(arrList.capacity+"\n");
+        arrList.add(4,99);
+        System.out.print(arrList.toString()+"\n");
+
+    // Test Case 4: Remove one element from the list at a specific index
+        System.out.print("\nTest case 4:\n");
+        
+        arrList.remove(4);
+        System.out.print(arrList.toString()+"\n");
+        
+    // Test Case 5: Print the list capacity, then increase it using the grow() method
+        System.out.print("\nTest case 5:\n");
+       
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
         arrList.grow();
-        System.out.print(arrList.capacity+"\n");
-        System.out.print(arrList.toString()+"\n");
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+        arrList.grow();
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+        arrList.grow();
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
         
-        for(int i=0; i<5; i++) {
-            arrList.add(i);
-        }
+    // Test Case 6: Print the list capacity, then decrease it using the shrink() method
+        System.out.print("\nTest case 6:\n");
         
-        //arrList.get(arrList.size());
-        //arrList.shrink();
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
         arrList.shrink();
-        System.out.print(arrList.capacity+"\n");
-        System.out.print(arrList.toString()+"\n");
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+        arrList.shrink();
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+        arrList.shrink();
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+        arrList.shrink();
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+
         
-        arrList.add(1);
-        System.out.print(arrList.capacity+"\n");
+    // Test Case 7: Set the value at a specific index in the list
+        System.out.print("\nTest case 7:\n");        
+        
+        arrList.set(6, 99);
         System.out.print(arrList.toString()+"\n");
+       
+    // Test Case 8: Show the capacity change when size exceeds the capacity
+        System.out.print("\nTest case 8:\n");
+        
+        System.out.print(arrList.toString()+"\n");
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n\n");
+        arrList.add(250);
+        
+        System.out.print(arrList.toString()+"\n");
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+        
+    // Test Case 9: Show the change in list size as the list capacity is decreased
+        System.out.print("\nTest case 9:\n");
+        
+        System.out.print(arrList.toString()+"\n");
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n\n");
         
         arrList.shrink();
-        System.out.print(arrList.capacity+"\n");
+        
         System.out.print(arrList.toString()+"\n");
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n\n");
         
         arrList.shrink();
-        System.out.print(arrList.capacity+"\n");
+        
         System.out.print(arrList.toString()+"\n");
+        System.out.print("The capacity of the ArrayList is: "+arrList.capacity+"\n");
+        
+     // Test Case 10: Use the get() method to retrieve data at a specific index
+        System.out.print("\nTest case 10:\n");
+        
+        System.out.println(arrList.toString()+"\n");
+        System.out.println("The value at index 2 is "+arrList.get(2));
+        System.out.println("The value at index 0 is "+arrList.get(0));
+        System.out.println("The value at index 4 is "+arrList.get(4));
+       
+
+        
+       
 
 
     } // end main() method
